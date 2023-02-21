d2dff9ba7cd2d33194a24015401f2422e81f5906
ProjectosILDO7 <marques2cuema@gmail.com>
2023-02-21 12:57:00 +0000

    commit para gravar

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,9 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./configRouter.js"
+import store from '../store/index'
+import { TOKEN_NAME } from "../configs/index.js";
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
   });
+
+router.beforeEach(async (to, from, next)=>{
+  const authorization = store.state.authorization
+  if(to.name){
+        return router.push({name:'home'})
+  }
+  next()
+
+})  
   
 export default router;
